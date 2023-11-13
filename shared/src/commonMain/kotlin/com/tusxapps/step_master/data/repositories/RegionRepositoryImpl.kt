2a63ae727d8a06e9dfa5ef1f61b523decc5ab9c8
tusxd <tusxdie@gmail.com>
2023-11-13 09:14:57 +0700

    Suspend run catching in RegionRepo

--- shared/src/commonMain/kotlin/com/tusxapps/step_master/data/repositories/RegionRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/com/tusxapps/step_master/data/repositories/RegionRepositoryImpl.kt
@@ -2,12 +2,13 @@ package com.tusxapps.step_master.data.repositories
 
 import com.tusxapps.step_master.data.network.API
 import com.tusxapps.step_master.domain.region.RegionRepository
+import com.tusxapps.step_master.utils.suspendRunCatching
 
 class RegionRepositoryImpl(
     private val api: API
 ) : RegionRepository {
-    override suspend fun getRegions(): Result<List<String>> = runCatching {
+    override suspend fun getRegions(): Result<List<String>> = suspendRunCatching {
         val regions = api.getRegions().result
-        return Result.success(regions.map { it.fullName })
+        regions.map { it.fullName }
     }
 }
